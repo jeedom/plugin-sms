7a16c5988c92ea3420657d4aa6307990b3eb251d
Loic <loicgevrey@gmail.com>
2014-11-15 16:31:11 +0100

    Bugfix condition check

--- core/class/sms.class.php
+++ core/class/sms.class.php
@@ -103,7 +103,7 @@ class sms extends eqLogic {
         if (posix_getsid(intval(trim(file_get_contents($pid_file))))) {
             return true;
         } else {
-            if (!file_exists($pid_file)) {
+            if (file_exists($pid_file)) {
                 unlink($pid_file);
             }
             return false;
