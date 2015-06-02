b7cdde5d65e8b1f1e9d9cdf3d8feef20f675039a
loic <lgevrey@oxya.com>
2015-06-02 10:25:44 +0200

    Improve update

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -19,15 +19,18 @@
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 
 function sms_update() {
-	$pid_file = dirname(__FILE__) .'/../../../tmp/sms.pid';
+	$pid_file = dirname(__FILE__) . '/../../../tmp/sms.pid';
 	if (file_exists($pid_file)) {
-		rename($pid_file,'/tmp/sms.pid');
+		rename($pid_file, '/tmp/sms.pid');
 	}
 	try {
 		sms::stopDeamon();
 		sms::runDeamon();
-	} catch (Exception  $e) {
-		
+	} catch (Exception $e) {
+
+	}
+	foreach (sms::byType('sms') as $sms) {
+		$sms->save();
 	}
 }
 
