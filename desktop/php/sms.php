c18a7c27809e3b41c14a63840b8e882e10baef2b
Loïc <loic@jeedom.com>
2016-07-28 18:18:48 +0200

    Improve UI

--- desktop/php/sms.php
+++ desktop/php/sms.php
@@ -55,6 +55,16 @@ foreach ($eqLogics as $eqLogic) {
 </div>
 
 <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
+ <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+   <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+
+   <ul class="nav nav-tabs" role="tablist">
+    <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
+    <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
+</ul>
+
+<div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+    <div role="tabpanel" class="tab-pane active" id="eqlogictab">
   <form class="form-horizontal">
     <fieldset>
       <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Générale}}  <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
@@ -88,8 +98,10 @@ foreach (object::all() as $object) {
    </fieldset>
  </form>
 
- <legend><i class="fa fa-list-alt"></i>  {{Commandes}}</legend>
- <a class="btn btn-success btn-sm cmdAction" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter d'un numéro}}</a><br/><br/>
+
+</div>
+<div role="tabpanel" class="tab-pane" id="commandtab">
+ <a class="btn btn-success btn-sm cmdAction pull-right" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter d'un numéro}}</a><br/><br/>
  <table id="table_cmd" class="table table-bordered table-condensed">
   <thead>
     <tr>
@@ -104,15 +116,8 @@ foreach (object::all() as $object) {
 
   </tbody>
 </table>
-
-<form class="form-horizontal">
-  <fieldset>
-    <div class="form-actions">
-      <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-      <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-    </div>
-  </fieldset>
-</form>
+</div>
+</div>
 
 </div>
 </div>
