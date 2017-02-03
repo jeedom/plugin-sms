5ceb95eb158dce41ec96cb22809dc9f3b4ed40fa
Loïc <loic@jeedom.com>
2017-02-03 10:50:31 +0100

    improve security

--- core/php/jeeSMS.php
+++ core/php/jeeSMS.php
@@ -18,7 +18,11 @@
 require_once dirname(__FILE__) . "/../../../../core/php/core.inc.php";
 
 if (!jeedom::apiAccess(init('apikey'), 'sms')) {
-	echo 'Clef API non valide, vous n\'etes pas autorisé à effectuer cette action (jeeZwave)';
+	echo __('Clef API non valide, vous n\'êtes pas autorisé à effectuer cette action (jeeSMS)', __FILE__);
+	die();
+}
+if (getClientIp() != '127.0.0.1') {
+	echo __('Clef API non valide, vous n\'êtes pas autorisé à effectuer cette action (jeeSMS)', __FILE__);
 	die();
 }
 
