ac9b83235dd4dbc264eb6da4e32c59b842fce2c6
Loïc <loic@jeedom.com>
2017-08-09 10:48:35 +0200

    Fix js error

--- desktop/php/sms.php
+++ desktop/php/sms.php
@@ -28,18 +28,18 @@ foreach ($eqLogics as $eqLogic) {
    <div class="eqLogicThumbnailContainer">
     <div class="cursor eqLogicAction" data-action="add" style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
       <i class="fa fa-plus-circle" style="font-size : 6em;color:#94ca02;"></i>
-    <br>
-    <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02">Ajouter</span>
-  </div>
-  <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
+      <br>
+      <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02">Ajouter</span>
+    </div>
+    <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
       <i class="fa fa-wrench" style="font-size : 6em;color:#767676;"></i>
-    <br>
-    <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676">{{Configuration}}</span>
+      <br>
+      <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676">{{Configuration}}</span>
+    </div>
   </div>
-</div>
-<legend><i class="fa fa-table"></i> {{Mes SMS}}</legend>
-<div class="eqLogicThumbnailContainer">
-  <?php
+  <legend><i class="fa fa-table"></i> {{Mes SMS}}</legend>
+  <div class="eqLogicThumbnailContainer">
+    <?php
 foreach ($eqLogics as $eqLogic) {
 	$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
 	echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="text-align: center; background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
@@ -49,7 +49,7 @@ foreach ($eqLogics as $eqLogic) {
 	echo '</div>';
 }
 ?>
-</div>
+ </div>
 </div>
 
 <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
@@ -57,68 +57,68 @@ foreach ($eqLogics as $eqLogic) {
   <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
   <a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a>
   <ul class="nav nav-tabs" role="tablist">
-    <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
-    <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
-    <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
-  </ul>
-  <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
-    <div role="tabpanel" class="tab-pane active" id="eqlogictab">
-      <br/>
-      <form class="form-horizontal">
-        <fieldset>
-          <div class="form-group">
-            <label class="col-sm-2 control-label">{{Nom de l'équipement SMS}}</label>
-            <div class="col-sm-3">
-              <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-              <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement GSM}}" />
-            </div>
+   <li role="presentation"><a class="eqLogicAction cursor" aria-controls="home" role="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
+   <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
+   <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
+ </ul>
+ <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+  <div role="tabpanel" class="tab-pane active" id="eqlogictab">
+    <br/>
+    <form class="form-horizontal">
+      <fieldset>
+        <div class="form-group">
+          <label class="col-sm-2 control-label">{{Nom de l'équipement SMS}}</label>
+          <div class="col-sm-3">
+            <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+            <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement GSM}}" />
           </div>
-          <div class="form-group">
-            <label class="col-sm-2 control-label"></label>
-            <div class="col-sm-9">
-              <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
-              <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
-            </div>
+        </div>
+        <div class="form-group">
+          <label class="col-sm-2 control-label"></label>
+          <div class="col-sm-9">
+            <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
+            <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
           </div>
-          <div class="form-group">
-            <label class="col-sm-2 control-label" >{{Objet parent}}</label>
-            <div class="col-sm-3">
-              <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
-                <option value="">{{Aucun}}</option>
-                <?php
+        </div>
+        <div class="form-group">
+          <label class="col-sm-2 control-label" >{{Objet parent}}</label>
+          <div class="col-sm-3">
+            <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+              <option value="">{{Aucun}}</option>
+              <?php
 foreach (object::all() as $object) {
 	echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
 }
 ?>
-             </select>
-           </div>
+           </select>
          </div>
-	<div class="form-group">
-		<label class="col-sm-2 control-label">{{Désactiver les intéractions}}</label>
-            	<div class="col-sm-9">
-			<input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="disableInteract" />
-            	</div>
-	</div>		
-       </fieldset>
-     </form>
-   </div>
-   <div role="tabpanel" class="tab-pane" id="commandtab">
-     <a class="btn btn-success btn-sm cmdAction pull-right" data-action="add" style="margin-top:5px;"><i class="fa fa-plus-circle"></i> {{Ajouter d'un numéro}}</a><br/><br/>
-     <table id="table_cmd" class="table table-bordered table-condensed">
-      <thead>
-        <tr>
-          <th>{{Nom}}</th>
-          <th>{{Utilisateur}}</th>
-          <th>{{Numéro}}</th>
-          <th>{{Paramètres}}</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
+       </div>
+       <div class="form-group">
+        <label class="col-sm-2 control-label">{{Désactiver les intéractions}}</label>
+        <div class="col-sm-9">
+         <input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="disableInteract" />
+       </div>
+     </div>
+   </fieldset>
+ </form>
+</div>
+<div role="tabpanel" class="tab-pane" id="commandtab">
+ <a class="btn btn-success btn-sm cmdAction pull-right" data-action="add" style="margin-top:5px;"><i class="fa fa-plus-circle"></i> {{Ajouter d'un numéro}}</a><br/><br/>
+ <table id="table_cmd" class="table table-bordered table-condensed">
+  <thead>
+    <tr>
+      <th>{{Nom}}</th>
+      <th>{{Utilisateur}}</th>
+      <th>{{Numéro}}</th>
+      <th>{{Paramètres}}</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
 
-      </tbody>
-    </table>
-  </div>
+  </tbody>
+</table>
+</div>
 </div>
 
 </div>
