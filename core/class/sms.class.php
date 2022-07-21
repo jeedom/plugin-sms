88b8411077f28273177cd4ca50e4f1949ff44cf3
Mips2648 <mips2648@gmail.com>
2022-07-21 12:59:26 +0200

    fix dependencies

--- core/class/sms.class.php
+++ core/class/sms.class.php
@@ -51,7 +51,7 @@ class sms extends eqLogic {
 	public static function dependancy_info() {
 		$return = array();
 		$return['progress_file'] = jeedom::getTmpFolder('sms') . '/dependance';
-		if (exec(system::getCmdSudo() . system::get('cmd_check') . '-E "python\-serial|python\-request|python\-pyudev" | wc -l') >= 3) {
+		if (exec(system::getCmdSudo() . system::get('cmd_check') . '-E "python3\-serial|python3\-request|python3\-pyudev" | wc -l') >= 3) {
 			$return['state'] = 'ok';
 		} else {
 			$return['state'] = 'nok';
